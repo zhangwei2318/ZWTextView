de4d2af79b0815fdc4b9cd6f8c68e5a604195c6a
zhangwei2318 <zhangwei2318@163.com>
2021-12-09 14:48:05 +0800

    no message

--- ZWTextView/Classes/YXTextView.swift
+++ ZWTextView/Classes/YXTextView.swift
@@ -102,75 +102,6 @@ public class ZWTextView: UIView {
             make.right.bottom.equalTo(-11)
         }
     }
-    /**
-     属性设置方法
-     * fontSize              文字大小, 默认15
-     * placeholder           占位符
-     * max            最大输入位数
-     * textColor             文字颜色, 默认黑色
-     * placeholderColor      占位符颜色, 默认浅灰
-     * maxColor        最多文字颜色, 默认浅灰
-     * TextChange      文本改变回调
-     * textEndEditing  文本结束编辑回调
-     */
-//    func set(fontSize: CGFloat?, placeholder: String?, max: Int?, textColor: UIColor?, placeholderColor: UIColor?, maxColor: UIColor?, textChange:TextChange, textEndEditing: TextEndEditing) {
-//        backgroundColor = .color238238238
-//        addSubview(textView)
-//        addSubview(textPlView)
-//        addSubview(maxNumLabel)
-//
-//        textView.snp.makeConstraints { (make) in
-//            make.left.top.equalTo(5)
-//            make.right.bottom.equalTo(-5)
-//        }
-//
-//        textPlView.snp.makeConstraints { (make) in
-//            make.left.right.top.bottom.equalTo(textView)
-//        }
-//
-//        maxNumLabel.snp.makeConstraints { (make) in
-//            make.right.bottom.equalTo(-11)
-//        }
-//
-//        textView.textColor = textColor
-//        textView.delegate = self
-//        // 文本改变
-//        textView.textChange {[weak self] (str) in
-//            var text = str
-//            self?.textPlView.isHidden = text.count != 0 ? true: false
-//            // 未超过最大值
-//            if let max = max, text.count <= max {
-//                self?.maxNumLabel.text = "\(text.count)/\(max)"
-//            }
-//            // 超过最大值截取
-//            if self?.textView.markedTextRange == nil, let max = max, text.count > max {
-//                text = String(text.prefix(max))
-//                self?.textView.text = text
-//                self?.maxNumLabel.text = "\(max)/\(max)"
-//            }
-//
-//            guard let block = textChange else {
-//                return
-//            }
-//            block(text)
-//        }
-//
-//        textView.textDidEndEditing(type: self) { (str) in
-//            guard let block = textEndEditing else {
-//                return
-//            }
-//            block(str)
-//        }
-//
-//        textPlView.text = placeholder
-//        textPlView.textColor = placeholderColor ?? .lightGray
-//
-//        maxNumLabel.textColor = maxColor ?? .lightGray
-//        guard let max = max else {
-//            return maxNumLabel.isHidden = true
-//        }
-//        maxNumLabel.text = "0/\(max)"
-//    }
      
     
     lazy var textView: UITextView = {
@@ -232,7 +163,6 @@ extension ZWTextView: UITextViewDelegate {
         }
         block(text)
     }
-              
               
 }
 
